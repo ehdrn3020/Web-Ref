c84a2ed7809f4a1bca94df0104bb07b1262ac04e
DongGu Kang <ehdrnsla3020@naver.com>
2018-03-25 15:17:48 +0900

    Update Express first.js

--- NodeJs/Express/Express first.js
+++ NodeJs/Express/Express first.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+//static file directory 'public'
+app.use(express.static('public'));
+
+//Routing
 app.get('/', function (req, res){
   res.send('hello world!');
 });
 
+//Listen
 app.listen(3000, function(){
   console.log("ex listening 3000 port!");
 });
