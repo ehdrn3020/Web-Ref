98aefe14195c32ccf09b6e26c5b6e9f36d67204e
DongGu Kang <ehdrnsla3020@naver.com>
2018-03-26 21:43:26 +0900

    Create start_jade.js

---
+++ NodeJs/Express/start_jade.js
@@ -0,0 +1,31 @@
+//cmd : npm install jade --save
+//set jade engine : app.set('view engine', 'jade');
+//set jade dir    : app.set('views','./views');   @make directory in nodeJS code
+//set jade rendering : res.reder('jade file name');
+
+
+[index.js]
+var express = require('express');
+var app = express();
+app.set('view engine', 'jade');
+app.set('views','./views')
+
+app.get('/', function (req, res){
+  res.send('hello world!');
+});
+app.get('/template', function(req, res){
+  res.render('temp');
+});
+app.listen(3000, function(){
+  console.log("ex listening 3000 port!");
+});
+
+
+[temp.jade]
+html
+  head Kang.DG
+  body
+    h1 hello & jade
+    ul
+      - for(var i=0; i<3; i++)
+        li coding jade
